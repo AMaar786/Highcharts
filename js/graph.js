08f9a6a95c5aa8cf62425f307886f3bd5cb02192
Shahrukh Khan <sk28671@gmail.com>
2016-07-28 15:38:06 +0500

    dsaas

--- js/graph.js
+++ js/graph.js
@@ -1,101 +1,35 @@
-/**
- * Created by Shahrukh khan on 7/28/2016.
- */
-
-var lengths = <%= data.length %>;
-
-
 $(function () {
-    var total = <%= total %>;
-    $('#competitors').highcharts({
+
+var options = {
         chart: {
-            type: 'pie',
-            height: '300'
+            rendedTo: 'barchart',
+            type: 'column',
+            width: 765
         },
-        credits: {
+        xAxis: {
+            labels: {
+                rotation: -45,
+                align: 'right'
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Nombre de sous-titres postés ce mois'
+            }
+        },
+        legend: {
             enabled: false
         },
-        title: {
-            text: ''
-        },
-        legends: { enabled: true
-        },
-        // Tool Tip
         tooltip: {
-            formatter: function () {
-                return '<b>Name :</b> ' + this.point.name + '<br/> <b>Tickets</b>: ' + this.y + '</b>';
-
-            }
-        },
-
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                // data Labels
-                dataLabels: {
-                    enabled: true,
-                    formatter: function () {
-                        return Highcharts.numberFormat(100 * this.y / total,0) + "%";
-
-                    },
-
-                    style:
-                    {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-
-                },
-                // data labels
-                showInLegend:true   // Legend Enable
-
-            },
-            series: {
-                cursor: 'pointer',
-                point: {
-                    events:
-                    {
-                        click: function()
-                        {
-                            window.location.href = this.url;
-                        }
-                    }
-                },
-                showInLegend: true //Legend Enable
-            }
-        },
-
-        // Axis Labels
-
-        xAxis: {
-
-            type: 'category',
-            labels: {
-                formatter: function () {
-                    var str = this.value;
-                    if (str.length < 21)
-                    { return str;}
-                    else
-                    {  return str.substring(0, 19)+"...";}
-                },
-                rotation: -45,
-                style: {
-                    fontSize: '11px'
-                }
-            }
-        },
-
-        yAxis: {
-            title: {
-                text: ''
+            formatter: function() {
+                var s = (this.y > 1) ? 's' : '';
+                return '<b>'+ this.x +'</b><br/>'+
+                     this.y+' sous-titre'+s+' posté'+s;
             }
         },
         series: [{
-            type: 'pie',
-            name: 'Tickets',
-            data: <%= data.to_json.html_safe %>
-    }]
+            name: 'Nombre d\'upload'
+        }]
+    }
 });
-});
-
-
